bca21f9866382fe4967420fa53ac6a7fd4bfba85
sawmue <sb@schickma.de>
2025-01-29 13:46:32 +0100

    fix: optimize for edge runtime

--- next.config.js
+++ next.config.js
@@ -9,6 +9,13 @@ const nextConfig = {
   },
   eslint: {
     ignoreDuringBuilds: false
+  },
+  experimental: {
+    serverActions: true,
+  },
+  env: {
+    POSTGRES_PRISMA_URL: process.env.POSTGRES_PRISMA_URL,
+    POSTGRES_URL_NON_POOLING: process.env.POSTGRES_URL_NON_POOLING
   }
 };
 
