1084693427ca6e04624a4e956c7a9e69888471ad
sawmue <sb@schickma.de>
2025-01-29 16:10:24 +0100

    fix: optimize prisma for edge functions

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,20 +1,14 @@
 import { PrismaClient } from '@prisma/client/edge'
-import { Pool } from '@neondatabase/serverless'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
 const connectionString = process.env.POSTGRES_PRISMA_URL || process.env.POSTGRES_URL_NON_POOLING || ''
-const apiKey = new URL(connectionString).password
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient({
   log: ['error'],
-  datasources: {
-    db: {
-      url: `prisma://proxy.proxy.neon.tech?api_key=${apiKey}`
-    }
-  }
+  datasourceUrl: connectionString
 })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma 
