39c991b63cf4fb46b67058fa08957321858e1578
sawmue <sb@schickma.de>
2025-01-29 17:09:32 +0100

    fix: set default theme to light

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -10,7 +10,7 @@ interface ProvidersProps {
 
 export default function Providers({ children }: ProvidersProps) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+    <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
       <ToastProvider>
         {children}
       </ToastProvider>
