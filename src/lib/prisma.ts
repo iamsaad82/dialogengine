4cedd718ff2f675773cd13057b38d4d477d38e2c
sawmue <sb@schickma.de>
2025-01-29 16:15:35 +0100

    fix: switch to regular serverless functions

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,11 @@
-import { PrismaClient } from '@prisma/client/edge'
+import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-const connectionString = process.env.POSTGRES_PRISMA_URL || process.env.POSTGRES_URL_NON_POOLING || ''
-
 export const prisma = globalForPrisma.prisma ?? new PrismaClient({
-  log: ['error'],
-  datasourceUrl: connectionString
+  log: ['error']
 })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma 
