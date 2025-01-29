7f7ce87eece5e206bb6c1488dd7b4f32d48f861e
sawmue <sb@schickma.de>
2025-01-29 13:01:49 +0100

    fix: optimize prisma client initialization

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,14 @@
 import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+let prisma: PrismaClient
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
+} else {
+  if (!(global as any).prisma) {
+    (global as any).prisma = new PrismaClient()
+  }
+  prisma = (global as any).prisma
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma 
+export { prisma } 
