bd679e379fffb42721748efc78f7de9578285a2e
sawmue <sb@schickma.de>
2025-01-30 10:10:31 +0100

    fix: Image-Konfiguration für lokale Entwicklung und Deployment angepasst

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['dialog-engine.vercel.app'],
+    domains: ['dialog-engine.vercel.app', 'localhost', '127.0.0.1'],
     unoptimized: true
   },
   typescript: {
